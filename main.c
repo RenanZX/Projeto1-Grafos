4110b054578bb3199094647217e860ad518876a7
Renan Godoi <renangodoimedeiros@gmail.com>
2017-09-03 17:20:39 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -9,13 +9,27 @@ int main(){
 	InsereVertice("b",2,grafo);
 	InsereVertice("c",3,grafo);
 	InsereVertice("d",4,grafo);
+	InsereVertice("e",5,grafo);
+	InsereVertice("f",6,grafo);
 	InsereAresta(1,2,grafo);
+	InsereAresta(1,3,grafo);
 	InsereAresta(1,4,grafo);
+	InsereAresta(1,5,grafo);
 	InsereAresta(2,3,grafo);
-	InsereAresta(3,1,grafo);
-	InsereAresta(3,2,grafo);
-	imprimirgrafo(grafo);
+	InsereAresta(2,4,grafo);
+	InsereAresta(2,5,grafo);
+	InsereAresta(2,6,grafo);
+	InsereAresta(3,4,grafo);
+	InsereAresta(3,5,grafo);
+	InsereAresta(4,5,grafo);
+	InsereAresta(5,6,grafo);
+	//listaVertices* gr = getCliqueMaximal(grafo,5);
+	//imprimirgrafo(gr);
+	//RemoveVertice(6,grafo);
+	//imprimirgrafo(grafo);
+	printf("vertice removido\n");
 	ExcluirGrafo(grafo);
+	//ExcluirGrafo(gr);
 	if(ListaVazia(grafo) == 1){
 		printf("grafo vazio\n");
 	}
