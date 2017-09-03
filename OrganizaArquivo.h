d1f4ec2e0c98e151a77a67e4bfd7fcf47c8f32e7
gabriel-alcantara <gabrielalcantaramourao@gmail.com>
2017-09-03 17:51:10 -0300

    oloco

--- OrganizaArquivo.h
+++ OrganizaArquivo.h
@@ -2,20 +2,10 @@
 #define _H_ORGANIZAARQUIVO
 
 #include "grafo.h"
-
-typedef struct {
-  int vert, qtd_arestas;
-  int arestas[51];
-  char nome[50];
-}Aluno;
-
-Aluno* LerRegistros(Aluno* aluno);
-
-listaVertices* InsereVerticesLidos(listaVertices* grafo);
 /*
+listaVertices LerRegistros(string caminhoArquivo);//a implementar
 void ImprimirGrafoGrauDecrescente();//a implementar
 void ImprimirCliqueMaximal()//a implementar
 void ImprimirCliqueMaximo();//a implementar
 */
-
 #endif
