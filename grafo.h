7371dc26346b7c61d96f77f13cecae6e3243f8c5
Renan Godoi <renangodoimedeiros@gmail.com>
2017-09-05 00:03:30 -0300

    Add files via upload

--- grafo.h
+++ grafo.h
@@ -15,6 +15,8 @@ typedef struct Elemento //Elemento da lista de vertices contendo o ponteiro para
 	char nome[1000];
 	int matricula;
 	int nroadjacentes;
+	int cliquepego;
+	int marcado;
 	struct Elemento * proximo;
 	struct ElementoAdjacente * primeiroAdj;
 	struct ElementoAdjacente * ultimoAdj;
@@ -26,22 +28,30 @@ typedef struct listav //lista de vertices do grafo
 	elemento_vertice * ultimo;
 }listaVertices;
 
+int poscliques;
+
+listaVertices ** cliques;
+
 listaVertices* CriarLista(); //cria e retorna um grafo
 void InsereVertice(char * nome,int matricula,listaVertices *grafo); //Insere Vertices na lista do grafo
 void InsereAdjacente(int novoadjacentemat,int verticemat,listaVertices* listav); //tendo duas matriculas insere um novo elemento adjacente no vertice
-/*void BuscarVertice(Vertice vertice,listaVertices *grafo); //Busca um vertice no grafo
-*/
 int ExisteVertice(int mat,listaVertices *grafo); //verifica a existencia de vertices apartir da matricula do aluno no grafo
 int ExisteAdjacente(int v1,int adjacente,listaVertices *grafo); //verifica a existencia de adjacentes no vertice com matricula v no grafo
 int InsereAresta(int v1mat,int v2mat,listaVertices *grafo); //Insere uma Aresta do vertice V1 para V2 no grafo
 int ListaVazia(listaVertices *grafo); //Verifica se a lista de vertices está vazia
 int ListaAdjVazia(int v,listaVertices *grafo); //Verifica se a lista de vertices adjacentes em relação a v no grafo está vazia
 void ExcluirGrafo(listaVertices *grafo);//exclui o grafo
-//void RemoverAresta(int v1,int v2,listaVertices *grafo); //remove uma aresta (v1,v2) no grafo
-//void RemoveVertice(int v1,listaVertices* l); //remove um vertice do grafo
 void ImprimirGrafo(listaVertices *grafo); //imprime os valores do grafo na tela
+void RemoveVertice(int v,listaVertices* grafo);
+void RemoveInicio(listaVertices* grafo);
+void CopiarLista(listaVertices* l1 ,listaVertices* l2);
+listaVertices* UnirListas(listaVertices* l1,listaVertices* l2);
+//void BronkerBosch(listaVertices* P,listaVertices* R,listaVertices *X);
+listaVertices* IntersecionarListas(listaVertices* l1,listaVertices *l2);
+listaVertices* IntersecionarListas_v(listaVertices* l1,elemento_vertice* l2);
 int MedirGrau(int v,listaVertices *grafo); //Mede o grau de um vertice no grafo
 void InterligarVertices(listaVertices* l); //Interliga todos os vertices do grafo
+void ApagarMarcas(listaVertices* grafo);//apaga marcas do grafo
 listaVertices* getCliqueMaximal(listaVertices *grafo,int nrovertices); //retorna um clique maximal recebendo um grafo e um numero contendo a quantidade de vertices
 listaVertices* getCliqueMaximo(listaVertices *grafo); //retorna um clique maximo do grafo
 
